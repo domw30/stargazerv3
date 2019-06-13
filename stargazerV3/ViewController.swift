46fff51b0d437b433e4ce8389028515e44f367e0
domw30 <domw30@gmail.com>
2019-06-13 14:13:04 +0100

    update planeDetection and worldAlignment

---
+++ stargazerV3/ViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  stargazerV3
+//
+//  Created by Student on 13/06/2019.
+//  Copyright © 2019 Dominic. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = .horizontal
+        config.worldAlignment = .gravityAndHeading
+        
+        sceneView.session.run(config)
+        
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+        addSphere()
+    }
+    
+    func addSphere(){
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.009))
+        sphere.position = SCNVector3(Float.random(in: 0 ..< 1),1 ,Float.random(in: 0 ..< 1))
+        sceneView.scene.rootNode.addChildNode(sphere)
+    }
+    
+}
+
